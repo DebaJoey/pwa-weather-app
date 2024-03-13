d00c2936cf3a151373185d644cd51e00bb518119
Joshua Debazar <debajoshua@gmail.com>
2024-03-13 15:39:15 +0000

    Implemented Service Workers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeatherComponent } from './weather/weather.component';
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule} from '@angular/material/icon'
+import { MatInputModule} from '@angular/material/input';
+import { ServiceWorkerModule } from '@angular/service-worker'
+import { MatSnackBarModule} from '@angular/material/snack-bar'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatSnackBarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
-    provideAnimationsAsync()
+
   ],
   bootstrap: [AppComponent]
 })
